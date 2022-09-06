daff2a0b8744db0b3ea877609d128ecf52a0cd10
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-06 20:27:42 +0100

    Removed all React logos e.t.c and created functionality for increase and decrease

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [increment, setIncrement] = useState(0);
+
+  function increase() {
+    setIncrement(increment + 1);
+  }
+
+  function decrease() {
+    setIncrement(increment - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="counter" onClick={increase}>
+        +
+      </button>
+      <button className="counter" onClick={decrease}>
+        -
+      </button>
+      <h1>{increment}</h1>
     </div>
   );
 }
